2ea7fdc37543f665f8ed59c3bee9d53f40167dd8
Mathe Guillaume (DI) <guillaume.mathe@etat.ge.ch>
2021-09-30 11:10:52 +0200

    Fix champ en trop dans BrouillonDemarche

--- src/main/java/ch/ge/ael/enu/business/domain/v1_0/BrouillonDemarche.java
+++ src/main/java/ch/ge/ael/enu/business/domain/v1_0/BrouillonDemarche.java
@@ -10,21 +10,17 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.net.URL;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BrouillonDemarche extends MessageENU {
-
     @NotBlank
     public String idDemarcheSiMetier;
-
-    @JsonDeserialize(using = IsoDateTimeDeserializer.class)
-    public LocalDateTime dateActionRequise;
 
     @NotNull
     public URL urlAction;
 
+    @JsonDeserialize(using = IsoDateTimeDeserializer.class)
     public LocalDate dateEcheanceAction;
 }
