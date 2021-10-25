d3a3fc0af1d495fb39e68444d39593ae540f8455
Mathe Guillaume (DI) <guillaume.mathe@etat.ge.ch>
2021-10-25 18:37:34 +0200

    Fix annotation sur classe abstraite + Fix plugin Javadoc avec sources en Java8

--- src/main/java/ch/ge/ael/enu/business/domain/v1_0/MessageENU.java
+++ src/main/java/ch/ge/ael/enu/business/domain/v1_0/MessageENU.java
@@ -2,12 +2,11 @@ package ch.ge.ael.enu.business.domain.v1_0;
 
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
-import lombok.extern.jackson.Jacksonized;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
-@Jacksonized @SuperBuilder
+@SuperBuilder
 public abstract class MessageENU {
 
     @NotBlank
