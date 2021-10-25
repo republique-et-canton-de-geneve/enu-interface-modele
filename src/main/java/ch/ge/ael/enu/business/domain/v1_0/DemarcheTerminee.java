b3e6d84fd0c4150e8eb75e19d15d981c2023d1e5
Mathe Guillaume (DI) <guillaume.mathe@etat.ge.ch>
2021-10-25 11:21:06 +0200

    Cleanup POM

--- src/main/java/ch/ge/ael/enu/business/domain/v1_0/DemarcheTerminee.java
+++ src/main/java/ch/ge/ael/enu/business/domain/v1_0/DemarcheTerminee.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.net.URL;
 import java.time.LocalDateTime;
 
