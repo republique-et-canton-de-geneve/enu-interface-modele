048fc1788e12fe0ffd12ed909120a6731dc84e40
Mathe Guillaume (DI) <guillaume.mathe@etat.ge.ch>
2021-10-25 11:57:43 +0200

    Ajout champs pour interface Demarche

--- src/main/java/ch/ge/ael/enu/business/domain/v1_0/Demarche.java
+++ src/main/java/ch/ge/ael/enu/business/domain/v1_0/Demarche.java
@@ -1,5 +1,7 @@
 package ch.ge.ael.enu.business.domain.v1_0;
 
 public interface Demarche {
+    public String getIdPrestation();
+    public String getIdUsager();
     public String getIdDemarcheSiMetier();
 }
