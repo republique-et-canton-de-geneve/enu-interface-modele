060c65f7617777618a9db054a233d0338ee0ff9e
Mathe Guillaume (DI) <guillaume.mathe@etat.ge.ch>
2022-04-19 14:46:24 +0200

    Fix numéro version 1.0

--- src/main/java/ch/ge/ael/enu/business/domain/Version.java
+++ src/main/java/ch/ge/ael/enu/business/domain/Version.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Version {
-    public static final String V1_0 = "1.1";
+    public static final String V1_0 = "1.0";
     public static final String V1_1 = "1.1";
 
     public static final List<String> SUPPORTED_VERSIONS = Arrays.asList(V1_0,V1_1);
