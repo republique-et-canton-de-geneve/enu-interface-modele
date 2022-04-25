7e0fdf3838e6c6fb01a05d2aafa8bed823cfd09c
Mathe Guillaume (DI) <guillaume.mathe@etat.ge.ch>
2022-04-25 09:20:02 +0200

    Ajout champ date demande pour la demande de liste de préférences

--- src/main/java/ch/ge/ael/enu/business/domain/v1_1/DemandeListePreference.java
+++ src/main/java/ch/ge/ael/enu/business/domain/v1_1/DemandeListePreference.java
@@ -6,11 +6,19 @@ import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
 @Data
 @Jacksonized
 @SuperBuilder
 @JsonIgnoreProperties(ignoreUnknown = true)
 @ToString(doNotUseGetters = true)
 public class DemandeListePreference {
+    @NotBlank
     public String idPrestation;
+
+    @NotNull
+    public LocalDate dateDemande;
 }
