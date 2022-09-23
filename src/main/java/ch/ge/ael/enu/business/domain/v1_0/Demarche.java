e37f56ab2f5bdaf25c24c47e9e3e9de1046374e9
chattif <foued.chatti@etat.ge.ch>
2022-09-23 10:22:36 +0200

    suppression de la version 1.0

--- src/main/java/ch/ge/ael/enu/business/domain/v1_0/Demarche.java
+++
@@ -1,7 +0,0 @@
-package ch.ge.ael.enu.business.domain.v1_0;
-
-public interface Demarche {
-    public String getIdPrestation();
-    public String getIdUsager();
-    public String getIdDemarcheSiMetier();
-}
--- src/main/java/ch/ge/ael/enu/business/domain/v1_0/ReceptionChoix.java
+++
@@ -1,6 +0,0 @@
-package ch.ge.ael.enu.business.domain.v1_0;
-
-public enum ReceptionChoix {
-    ELECTRONIQUE,
-    TOUT
-}
