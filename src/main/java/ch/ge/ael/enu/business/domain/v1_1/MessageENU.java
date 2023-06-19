21c027c3f8ab4cd195e3606b3d7f864410ac3ed6
chattif <foued.chatti@etat.ge.ch>
2023-06-19 09:11:27 +0200

    ENU-2099 Ajouter le séquençage aux messages rabbit

--- src/main/java/ch/ge/ael/enu/business/domain/v1_1/MessageENU.java
+++ src/main/java/ch/ge/ael/enu/business/domain/v1_1/MessageENU.java
@@ -1,15 +1,18 @@
 package ch.ge.ael.enu.business.domain.v1_1;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
 
 @Data
 @SuperBuilder
 @ToString(doNotUseGetters = true)
-public abstract class MessageENU {
+@AllArgsConstructor
+public abstract class MessageENU implements Serializable {
 
     @NotBlank
     public String idPrestation;
